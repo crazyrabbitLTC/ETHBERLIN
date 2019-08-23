cc532b11dd771ba76c1e6df932280b50ec547c78
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-08-23 23:47:15 +0200

    Added PRettier Formating

--- contracts/WordDao.sol
+++ contracts/WordDao.sol
@@ -4,24 +4,23 @@ import "./WordToken.sol";
 import "@openzeppelin/upgrades/contracts/Initializable.sol";
 
 contract WordDao is Initializable {
+    WordToken public token;
+    uint256 public balance;
 
-  WordToken public token;
-  uint256 public balance;
-  
-  event wordAdded(string word, uint256 tribute, address adder);
+    event wordAdded(string word, uint256 tribute, address adder);
 
-  function SetupDao() public initializer {
-    token = new WordToken(450000);
-  }
+    function SetupDao() public initializer {
+        token = new WordToken(450000);
+    }
 
-  function addWord(string memory _word) public payable {
-    token.transfer(msg.sender, 1);
-    balance += msg.value;
-    emit wordAdded(_word, msg.value, msg.sender);
-  }
+    function addWord(string memory _word) public payable {
+        token.transfer(msg.sender, 1);
+        balance += msg.value;
+        emit wordAdded(_word, msg.value, msg.sender);
+    }
 
-function getBalance()public returns(uint256){
-  return address(this).balance;
-}
+    function getBalance() public returns (uint256) {
+        return address(this).balance;
+    }
 
 }
--- contracts/WordToken.sol
+++ contracts/WordToken.sol
@@ -4,7 +4,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract WordToken is ERC20, ERC20Detailed {
-    constructor(uint256 initialSupply) ERC20Detailed("WordToken", "WT", 18) public {
+    constructor(uint256 initialSupply)
+        public
+        ERC20Detailed("WordToken", "WT", 18)
+    {
         _mint(msg.sender, initialSupply);
     }
 }
