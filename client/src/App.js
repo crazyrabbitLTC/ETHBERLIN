c4ffc4bf0f05c94538899d7286df1b87b979ff19
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-08-23 23:12:39 +0200

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+
+import { useWeb3Network, useEphemeralKey, useWeb3Injected } from '@openzeppelin/network';
+import Header from './components/Header/index.js';
+import Footer from './components/Footer/index.js';
+import Hero from './components/Hero/index.js';
+import Web3Info from './components/Web3Info/index.js';
+import Counter from './components/Counter/index.js';
+
+import styles from './App.module.scss';
+
+// const infuraToken = process.env.REACT_APP_INFURA_TOKEN;
+const infuraToken = '95202223388e49f48b423ea50a70e336';
+
+function App() {
+  // get ephemeralKey
+  const signKey = useEphemeralKey();
+
+  // get GSN web3
+  // const context = useWeb3Network(`wss://rinkeby.infura.io/ws/v3/${infuraToken}`, {
+  //   pollInterval: 15 * 1000,
+  //   gsn: {
+  //     signKey,
+  //   },
+  // });
+
+  // const context = useWeb3Network('http://127.0.0.1:8545', {
+  //   gsn: {
+  //     dev: true,
+  //     signKey,
+  //   },
+  // });
+
+  const context = useWeb3Injected();
+
+  // load Counter json artifact
+  let counterJSON = undefined;
+  try {
+    counterJSON = require('../../contracts/Counter.sol');
+  } catch (e) {
+    console.log(e);
+  }
+
+  // load Counter instance
+  const [counterInstance, setCounterInstance] = useState(undefined);
+  let deployedNetwork = undefined;
+  if (!counterInstance && context && counterJSON && counterJSON.networks && context.networkId) {
+    deployedNetwork = counterJSON.networks[context.networkId.toString()];
+    if (deployedNetwork) {
+      setCounterInstance(new context.lib.eth.Contract(counterJSON.abi, deployedNetwork.address));
+    }
+  }
+
+  function renderNoWeb3() {
+    return (
+      <div className={styles.loader}>
+        <h3>Web3 Provider Not Found</h3>
+        <p>Please, install and run Ganache.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.App}>
+      <Header />
+      <Hero />
+      <div className={styles.wrapper}>
+        {!context.lib && renderNoWeb3()}
+        <div className={styles.contracts}>
+          <h1>BUIDL with GSN Kit!</h1>
+          <div className={styles.widgets}>
+            <Web3Info title="Web3 Provider" context={context} />
+            <Counter {...context} JSON={counterJSON} instance={counterInstance} deployedNetwork={deployedNetwork} />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ truffle-config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+require("dotenv").config();
+const mnemonic = process.env.MNEMONIC;
+const infuraKey = process.env.INFURA_API_KEY;
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          "https://ropsten.infura.io/v3/" + infuraKey
+        );
+      },
+      network_id: "3",
+      gas: 4465030,
+      gasPrice: 10000000000
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          "https://kovan.infura.io/v3/" + infuraKey
+        );
+      },
+      network_id: "42",
+      gas: 4465030,
+      gasPrice: 10000000000
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          "https://rinkeby.infura.io/v3/" + infuraKey
+        ),
+      network_id: 4,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+    // main ethereum network(mainnet)
+    main: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          "https://mainnet.infura.io/v3/" + infuraKey
+        ),
+      network_id: 1,
+      gas: 3000000,
+      gasPrice: 10000000000
+    }
+  }
+};
