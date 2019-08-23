bd4041effda0bb88fe15848e3e200502633c38e5
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-08-23 23:41:13 +0200

    Basic Scaffold for Contract

--- contracts/WordDao.sol
+++ contracts/WordDao.sol
@@ -6,9 +6,22 @@ import "@openzeppelin/upgrades/contracts/Initializable.sol";
 contract WordDao is Initializable {
 
   WordToken public token;
+  uint256 public balance;
+  
+  event wordAdded(string word, uint256 tribute, address adder);
 
   function SetupDao() public initializer {
     token = new WordToken(450000);
   }
+
+  function addWord(string memory _word) public payable {
+    token.transfer(msg.sender, 1);
+    balance += msg.value;
+    emit wordAdded(_word, msg.value, msg.sender);
+  }
+
+function getBalance()public returns(uint256){
+  return address(this).balance;
+}
 
 }
