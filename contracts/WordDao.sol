76dd2c538a2acbc5610f9813e1c2d91784beb214
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-09-07 13:20:59 +0200

    creating comments

--- contracts/WordDao.sol
+++ contracts/WordDao.sol
@@ -6,25 +6,51 @@ import "./WordStorage.sol";
 import "./WordToken.sol";
 
 contract WordDao is Initializable, Verify {
+
+    /********
+    Storage Variables
+    ********/
+
+    //Address of the ERC20 token that is given to users who contribute words
     WordToken public token;
+
+    //Address of the WordStorage that is created by the WordDao contract
     WordStorage public wordStorage;
 
+    //Balance of the contract. This should be set to a seperate contract (gnosis safe?)
     uint256 public contractBalance;
+
+    //The payment required to set a word.
     uint256 public tribute;
+
+    //The signature Authority that is required to have signed the words before adding them. 
     address public signAuthority;
+
+    //Address of the Dao contract which controls the WordDao
     address public DAOController;
+
+    //Address of the initial Administrator for the WordDao. Once setup, it should be transfered
+    //To the DAOController.
     address public owner;
 
+    //Public Getter for if a word Exists. This should either be extracted to the wordStorage,
+    //Or there should be availible functionlity to select by what contract you are addressing. 
+    //Arguments for leaving it here is reduced complexity for the Storage contract.
+    //Arguments for putting it on the storage contract is that it kind of 'belongs' there. 
     mapping(string => bool) public wordExists;
+    mapping(uint256 => string) wordMapping;
 
     /********
     MODIFIERS
     ********/
+
+    //This requires that contract be from the owner. Owner can be set  to be  MasterDao.
     modifier onlyMaster {
         require(msg.sender == owner, "WordDao:: Only Master DAO can Control");
         _;
     }
 
+    //To prevent Reentrancy  for withdrawls. Copied from Moloch Dao.
     bool locked;
     modifier noReentrancy() {
         require(!locked, "WordDao: Reentrant call");
@@ -33,7 +59,10 @@ contract WordDao is Initializable, Verify {
         locked = false;
     }
 
-    mapping(uint256 => string) wordMapping;
+
+    /********
+    EVENTS
+    ********/
 
     event wordAdded(
         string word,
@@ -43,22 +72,37 @@ contract WordDao is Initializable, Verify {
     );
     event daoMaster(address daoMaster);
 
-    //the Token should be mintable
-    function SetupDao(string memory _language, uint256 _fee, uint256 _tribute)
+
+    /********
+    FUNCTIONS
+    ********/
+
+    //WordDao Setup
+    function SetupDao(string memory _language, uint256 _fee, uint256 _tribute, uint256 _totalWordCount)
         public
         initializer
     {
-        token = new WordToken(450000);
-        wordStorage = new WordStorage(_language, _fee);
+        //Set inital contract values
         owner = msg.sender;
         tribute = _tribute;
+      //Setup the token with the tokens availible. This should not be fixed to allow for new words. 
+        token = new WordToken(_totalWordCount);
+      //Cast the address of this contract, the WordDao  to address payable
+        address payable fundRecipent = address(uint160(address(this)));
+      //Create a  new wordStorage
+        wordStorage = new WordStorage(_language, _fee, fundRecipent);
+      //Set the address that has the authority to sign new words
         signAuthority = address(0x76991b32A0eE1996E5c3dB5FdD29029882D587DF);
     }
 
+    //Set the controller of the WordDao.
     function setMaster(address _dao) public onlyMaster {
-        require(msg.sender == owner, "Only Owner can change dao Controller");
         DAOController = _dao;
         emit daoMaster(_dao);
+    }
+
+    function setOwnerToDao() public onlyMaster {
+      owner = DAOController;
     }
 
     function addWord(string memory _word, bytes memory signature)
