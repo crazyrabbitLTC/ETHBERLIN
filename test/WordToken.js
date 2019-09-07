c6b6a9810552663dbaa2301ef42d99b1fc4b585d
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-09-07 23:15:55 +0200

    Add solidity  coverage

--- test/WordToken.js
+++ test/WordToken.js
@@ -1,23 +1,26 @@
-const { BN, constants, expectEvent, shouldFail } = require('openzeppelin-test-helpers');
-const should = require('chai').should();
+const {
+  BN,
+  constants,
+  expectEvent,
+  shouldFail
+} = require("openzeppelin-test-helpers");
+const should = require("chai").should();
 
-const Counter = artifacts.require('Counter');
+const WordDao = artifacts.require("WordDao");
+const WordStorage = artifacts.require("WordStorage");
+const WordToken = artifacts.require("WordToken");
 
-const WordDao = artifacts.require('WordDao');
-const WordStorage = artifacts.require('WordStorage');
-const WordToken = artifacts.require('WordToken');
-
-contract("Word Token", async ([_, owner, ...otherAccounts])=> {
+contract("Word Token", async ([_, owner, ...otherAccounts]) => {
   let wordToken;
 
-  beforeEach(async function () {
+  beforeEach(async function() {
     wordToken = await WordToken.new(10);
-  })
+  });
 
   it("Should have the  WordToken Name", async () => {
-    (await wordToken.name()).should.equal('WordToken');
-  })
-})
+    (await wordToken.name()).should.equal("WordToken");
+  });
+});
 
 // contract("counter", async ([_, owner, ...otherAccounts]) => {
 //   let counter;
@@ -28,7 +31,7 @@ contract("Word Token", async ([_, owner, ...otherAccounts])=> {
 //     counter = await Counter.new();
 //     counter.initialize(value, { from: owner });
 //   });
-  
+
 //   it("should have proper owner", async () => {
 //     (await counter.owner()).should.equal(owner);
 //   });
@@ -43,4 +46,3 @@ contract("Word Token", async ([_, owner, ...otherAccounts])=> {
 //   });
 
 //});
-
