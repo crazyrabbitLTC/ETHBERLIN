100f2d064e6f25ab38a3cb7f9893eccf03102ddc
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-09-09 11:29:00 +0200

    Add Truffle Contraxct Size to Master

--- truffle-config.js
+++ truffle-config.js
@@ -64,5 +64,6 @@ module.exports = {
       gas: 0xfffffffffff, // <-- Use this high gas value
       gasPrice: 0x01 // <-- Use this low gas price
     }
-  }
+  },
+  plugins: ["truffle-contract-size"]
 };
